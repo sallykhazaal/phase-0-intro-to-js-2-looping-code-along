fdbe2fc8bc80dff29adb975e45d851877d9b5a3a
sallykhazaal <sallykhazaal@yahoo.com>
2023-01-19 13:50:53 -0500

    commit tree 97ef319a547fcc66c47c7562fbb45e32a35020bd

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+  console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+function writeCards(array, event) {
+  const newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    newArray.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`);
+  }
+  return newArray;
+}
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+function countDown(number) {
+  let countDown = 10;
+  while (countDown >= 0) console.log(countDown--);
+}
